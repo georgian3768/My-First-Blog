f64e58ec2531968e00db377902dfe74d39f4a45a
admin <georgian3768@gmail.com>
2023-12-25 03:04:01 +0530

    added and linked post_detail.html

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import post
 from django.utils import timezone
 
@@ -6,3 +6,7 @@ from django.utils import timezone
 def post_list(request):
     posts = post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post_detl = get_object_or_404(post, pk=pk)
+    return render(request,'blog/post_detail.html',{'post_detl':post_detl})
