39079095ec65f48a87d7528633129662d77948d6
admin <georgian3768@gmail.com>
2023-12-24 12:36:40 +0530

    Changes in url file

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+# Here we're importing Django's function path and
+# all of our views from the blog application.
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.post_list, name = 'post_list')    
+]
+
